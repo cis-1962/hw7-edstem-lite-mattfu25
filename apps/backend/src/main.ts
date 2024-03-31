a8668913e0b3c28ca16c3a9149d817a039190c64
Matt Fu <matthewfu21@gmail.com>
2024-03-31 16:04:20 -0400

    Setup router structure and add cookie-session middleware

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,16 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+import accountRouter from './routes/account';
 
 // read environment variables from .env file
 dotenv.config();
 const PORT = process.env.PORT ?? 8000;
 
 // connect to database
-mongoose.connect(process.env.MONGODB_URI, {});
+mongoose.connect(process.env.MONGODB_URI || '', {});
 
+// start express app
 const app = express();
 
+// add cookie middleware
+app.use(cookieSession({
+  name: 'session',
+  secret: process.env.COOKIE_SECRET,
+  maxAge: 24 * 60 * 60 * 1000,
+})); 
+
+// add json parser middleware
 app.use(express.json());
+
+// add account router
+app.use('/api/account', accountRouter);
 
 // define root route
 app.get('/api/hello', (_, res) => {
---
+++ apps/backend/src/routes/account.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const accountRouter = express.Router();
+
+// signup route
+accountRouter.post('/signup', (req, res) => {
+
+});
+
+// login route
+accountRouter.post('/login', (req, res) => {
+
+});
+
+// logout route
+accountRouter.post('/logout', (req, res) => {
+
+});
+
+export default accountRouter;
